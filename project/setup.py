297b8c07f2c881bb3d9e335741f9e51808532783
ShamHolder <holdenmark3@gmail.com>
2021-10-25 12:13:56 -0400

    Update setup.py

--- project/setup.py
+++ project/setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup
 
+setup(
+  # Silence Warnings
+  name='Machine Learning Algorithms',
+  url='https://github.com/ShamHolder/ML-Algorithms/tree/main/project',
+  author='Shane Holden',
+  author_email='holdenmark3@gmail.com',
+  packages=['msh_machinelearning'],
+  install_requires=['numpy'],
+  version='0.1',
+  license='MIT',
+  description='Python package with classification algorithms for machine learning',
+  long_description=open('README.md').read()
+)
+  
+  
